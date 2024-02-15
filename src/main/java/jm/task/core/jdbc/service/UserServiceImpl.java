0578876325d85bed774519f4ce156352dd56681c
Lyksan-ruthevul <154101744+Lyksan-ruthevul@users.noreply.github.com>
2024-02-15 13:03:33 +0300

    Fix

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -9,31 +9,37 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     private UserDao userDaoImpl = new UserDaoJDBCImpl();
 
+    @Override
     public void createUsersTable() {
 
         userDaoImpl.createUsersTable();
     }
 
+    @Override
     public void dropUsersTable() {
 
         userDaoImpl.dropUsersTable();
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
 
         userDaoImpl.saveUser(name, lastName, age);
     }
 
+    @Override
     public void removeUserById(long id) {
 
         userDaoImpl.removeUserById(id);
     }
 
+    @Override
     public List<User> getAllUsers() {
 
         return userDaoImpl.getAllUsers();
     }
 
+    @Override
     public void cleanUsersTable() {
         userDaoImpl.cleanUsersTable();
     }
