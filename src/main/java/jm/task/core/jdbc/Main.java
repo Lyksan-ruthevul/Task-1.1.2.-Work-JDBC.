2b6d82b60c002995d8ae1489a023740761a933c8
Lyksan-ruthevul <154101744+Lyksan-ruthevul@users.noreply.github.com>
2024-02-15 10:14:50 +0300

    Практическая задача 1.1.2. Работа с базой данных c помощью JDBC.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.model.User;
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        User[] users = {
+                new User("Denis", "Pepsi", (byte) 24),
+                new User("Ivan", "Oldrus", (byte) 100),
+                new User("Sergeo", "Morello", (byte) 40),
+                new User("Dude", "Coolov", (byte) 12)};
+
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        for (User user : users) {
+            userService.saveUser(user.getName(), user.getLastName(), user.getAge());
+            System.out.printf("User с именем – %s добавлен в базу данных\n", user.getName());
+        }
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
